c5d17f66eef761e360c5ecbcd37c6ce257af6a6b
Saibou <saiboub80@gmail.com>
2023-05-06 00:41:18 -0400

    game, useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import MoleContainer from "./Compenents/MoleContainer";
+import startImage from './start.png'; // Import the image
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [isGameRunning, setIsGameRunning] = useState(false);
+
+  const createMoleHill = () => {
+    let hills = [];
+    for (let i = 0; i < 9; i++) {
+      hills.push(
+        <MoleContainer
+          key={i}
+          setScore={setScore}
+          score={score}
+        />
+      );
+    }
+
+    return (
+      <div>
+        {hills}
+      </div>
+    );
+  };
+
+  const handleStart = () => {
+    setIsGameRunning(true);
+  };
+
+  const handlePause = () => {
+    setIsGameRunning(false);
+  };
+
+  const handleReset = () => {
+    setScore(0);
+    setIsGameRunning(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="splash-image">
+        <img src={startImage} alt='Mole'/>
+      </div>
+     
+      <button
+        className="start-button"
+        onClick={handleStart}
+        disabled={isGameRunning}
+      >
+        Start
+      </button>
+      <button
+        className="pause-button"
+        onClick={handlePause}
+        disabled={!isGameRunning}
+      >
+        Pause
+      </button>
+      <button
+        className="reset-button"
+        onClick={handleReset}
+      >
+        Reset
+      </button>
+      
+      <p className="score">Score: {score}</p> {/* Display the current score */}
+      
+      {isGameRunning && createMoleHill()}
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
---
+++ src/Compenents/Mole.js
@@ -0,0 +1,23 @@
+import { useEffect } from 'react'
+import moleImg from '../mole.png'
+
+const Mole = (props) => {
+
+    useEffect(() => {
+        let randSeconds = Math.ceil(Math.random() * 5000)
+        let timer = setTimeout(() => {
+            props.toggle(false)
+        }, randSeconds)
+        return () => clearTimeout(timer)
+    })
+
+    return (
+        <div>
+            <img style={{'width': '30vw'}}
+             src={moleImg} alt="Mole"
+             onClick={props.handleClick} />
+        </div>
+    )
+}
+
+export default Mole
